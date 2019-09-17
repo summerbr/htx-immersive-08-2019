38e523303756dc42f148865689d933fb13abfef3
Summer Brown <summer-brown@comcast.net>
2019-09-17 13:40:27 -0500

    js101

---
+++ 04-week/0-monday/labs/summerbr/exercises/level1.js
@@ -0,0 +1,41 @@
+function lvl1exercise1 () {
+  // Declare a variable without instantiating it and return it!
+  var name;
+  return name;
+}
+
+function lvl1exercise2 () {
+  // Declare and instantiate a number and return it
+  var legalAge = 21;
+  return legalAge;
+}
+
+function lvl1exercise3 () {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+  var pi = 3.14159;
+  return pi;
+}
+
+function lvl1exercise4 () {
+  // Declare and instantiate a string "Hello World!" and return it
+  var x = "Hello World!";
+  return x;
+}
+
+function lvl1exercise5 () {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  var misc = ['Hello World!', 4];
+  return misc;
+}
+
+function lvl1exercise6 () {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  var misc = {key1:"Hello World!", key2:4};
+  return misc;
+}
+
+function lvl1exercise7 () {
+  // Declare and instantiate a boolean value 'false' and return it
+  var notTrue = false;
+  return notTrue;
+}
