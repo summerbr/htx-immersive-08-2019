ebca73be5189abbbe9c7b08e480b865989ac5ec9
Summer Brown <summer-brown@comcast.net>
2019-09-24 11:00:18 -0500

    hit button functionality added

--- 05-week/0-monday/labs/summerbr/blackjack.js
+++ 05-week/0-monday/labs/summerbr/blackjack.js
@@ -1,42 +1,33 @@
-// var deck = [];
-// var playerHand = [];
-// var dealerHand = [];
+var deck = [];
 
-// let suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
-// let values = ['Ace', 'King', 'Queen', 'Jack', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-// function createDeck() {}
+let suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
+let values = ['Ace', 'King', 'Queen', 'Jack', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+function createDeck() {}
 var deal = () => {
-    createCard('9_of_hearts', 'dealer')
-    createCard('9_of_hearts', 'dealer')
-    createCard('9_of_hearts', 'player')
-    createCard('9_of_hearts', 'player')
-    
-    // // deal 2 cards to dealer
-    // let dealerCard = document.createElement("img");
-    // let dealerCard2 = document.createElement("img");
-    // dealerCard.src= "images/9_of_hearts.png";
-    // dealerCard2.src="images/ace_of_diamonds.png";
-    // dealerCard.setAttribute("class", "card");
-    // dealerCard2.setAttribute("class", "card");
-    // document.getElementById("dealer-hand").appendChild(dealerCard);
-    // document.getElementById("dealer-hand").appendChild(dealerCard2);
-    // // deal 2 cards to player
-    // let playerCard = document.createElement("img");
-    // let playerCard2 = document.createElement("img");
-    // playerCard.src= "images/king_of_hearts.png";
-    // playerCard2.src="images/ace_of_spades.png";
-    // playerCard.setAttribute("class", "card");
-    // playerCard2.setAttribute("class", "card");
-    // document.getElementById("player-hand").appendChild(playerCard);
-    // document.getElementById("player-hand").appendChild(playerCard2);
+    createCard('9_of_hearts', 'dealer');
+    createCard('king_of_spades', 'dealer');
+    createCard('jack_of_clubs', 'player');
+    createCard('7_of_diamonds', 'player');
 }
-
 function createCard(value, person) {
     let card = document.createElement("img");
     card.src= "images/" + value + ".png";
     card.setAttribute("class", "card");
     document.getElementById(person + "-hand").appendChild(card);
 }
-
 document.getElementById("deal-button").addEventListener("click", deal);
 
+var hit = () => {
+    createCard('2_of_spades', 'player');
+}
+document.getElementById("hit-button").addEventListener("click", hit); 
+
+// hide deal button after first click
+// // setTimeout
+// $("deal-button").click(function () {
+//     $("deal-button").attr("disabled", true).delay(2000).attr("disabled", false);
+//   });
+
+// document.getElementById("hit-button").addEventListener("click", hit); Hit
+// document.getElementById("stand-button").addEventListener("click", stand); Stand
+// $("playAgain-button").hide();
